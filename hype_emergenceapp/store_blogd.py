fd41951147c7acef44f1aa8d10777ad68a1093e7
Ubuntu <ubuntu@ip-172-31-3-213.us-west-1.compute.internal>
2014-07-24 19:31:45 +0000

    some changes store sql

--- hype_emergenceapp/store_blogd.py
+++ hype_emergenceapp/store_blogd.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 
 def main():
 
-	engine = create_engine('postgresql://postgres:@localhost:5432/hypemerdb')
+	engine = create_engine('postgresql://postgres:hype@ip-172-31-3-213:5432/hypemerdb')
 	
 	blogd = bc.hypem_emergence(5)
 	blogd.features[['Results', 'img']].to_sql('hypemer_closet', engine, if_exists='append')
